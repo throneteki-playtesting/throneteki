27c1cb9922eacda4c081dcfbbe58ed01b37691ef
Duckbringer <34427714+Duckbringer@users.noreply.github.com>
2022-10-19 09:22:18 +0200

    Add cost and message to LordProtectorRetainer.js

--- server/game/cards/23-AHaH/LordProtectorsRetainer.js
+++ server/game/cards/23-AHaH/LordProtectorsRetainer.js
@@ -15,7 +15,12 @@ class LordProtectorsRetainer extends DrawCard {
                                                     selection.value.isMatch({ trait: ['Lord', 'Lady'], type: 'character' })
                                                 ))
             },
+            cost: ability.costs.returnSelfToHand(),
             max: ability.limit.perPhase(1),
+            message: {
+                format: '{player} uses {source} and returns it to hand to cancel {abilitySource}',
+                args: { abilitySource: context => context.event.source }
+            },
             gameAction: GameActions.cancelEffects(context => ({ event: context.event }))
         });
     }
