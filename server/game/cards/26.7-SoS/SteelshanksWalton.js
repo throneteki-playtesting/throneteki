26ee94dae064cd50d13abd1c2ae1f8cea3506fbd
Brian Upton <ystros@gmail.com>
2024-10-07 22:11:39 -0700

    Implement Steelshanks Walton v1.0.0

---
+++ server/game/cards/26.7-SoS/SteelshanksWalton.js
@@ -0,0 +1,32 @@
+import DrawCard from '../../drawcard.js';
+import GameActions from '../../GameActions/index.js';
+
+class SteelshanksWalton extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Take control',
+            target: {
+                cardCondition: {
+                    loyal: false,
+                    type: 'character',
+                    controller: 'opponent',
+                    condition: (card, context) => card.owner === context.player && GameActions.takeControl({ card, player: context.player }).allow()
+                }
+            },
+            handler: (context) => {
+                this.game.resolveGameAction(
+                    GameActions.takeControl((context) => ({
+                        card: context.target,
+                        player: context.player
+                    })),
+                    context
+                );
+            }
+        });
+    }
+}
+
+SteelshanksWalton.code = '26563';
+SteelshanksWalton.version = '1.0.0';
+
+export default SteelshanksWalton;
