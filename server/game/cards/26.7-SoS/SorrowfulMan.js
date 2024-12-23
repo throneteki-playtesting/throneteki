a2dce4770131da2c13097c1865306337927386eb
Stephen Patane <patane97@gmail.com>
2024-12-23 11:43:40 +1100

    Re-writing SorrowfulMan to fix it (completely broken)

--- server/game/cards/26.7-SoS/SorrowfulMan.js
+++ server/game/cards/26.7-SoS/SorrowfulMan.js
@@ -4,44 +4,51 @@ class SorrowfulMan extends DrawCard {
     setupCardAbilities() {
         this.reaction({
             when: {
-                onCardOutOfShadows: (event) => event.card === this
+                onCardOutOfShadows: (event) =>
+                    event.card === this &&
+                    // TODO: Clean target logic up on release
+                    this.game
+                        .getOpponents(this.controller)
+                        .some(
+                            (player) => player.discardPile.length > 0 && player.deadPile.length > 0
+                        )
             },
-            targets: {
-                discardCard: {
-                    cardCondition: {
-                        type: 'character',
-                        location: 'discard pile',
-                        condition: (card, context) => card.controller !== context.player
-                    }
-                },
-                deadCard: {
-                    cardCondition: {
-                        type: 'character',
-                        location: 'dead pile',
-                        condition: (card, context) =>
-                            !context.targets.discardCard ||
-                            card.controller === context.targets.discardCard.controller
-                    }
-                }
+            target: {
+                mode: 'exactly',
+                numCards: 2,
+                activePromptTitle: 'Select 2 characters',
+                singleController: true,
+                cardCondition: (card, context) =>
+                    card.controller !== this.controller &&
+                    ['discard pile', 'dead pile']
+                        .filter(
+                            (location) =>
+                                !context.selectedCards.some((card) => card.location === location)
+                        )
+                        .includes(card.location)
             },
             message: {
                 format: "{player} uses {source} to swap {discardCard} in {controller}'s discard pile with {deadCard} in their dead pile",
                 args: {
-                    discardCard: (context) => context.targets.discardCard,
-                    deadCard: (context) => context.targets.deadCard,
-                    controller: (context) => context.targets.discardCard.controller
+                    discardCard: (context) =>
+                        context.target.find((card) => card.location === 'discard pile'),
+                    deadCard: (context) =>
+                        context.target.find((card) => card.location === 'dead pile'),
+                    controller: (context) => context.target[0].controller
                 }
             },
             handler: (context) => {
-                const opponent = context.targets.discardCard.controller;
-                const discardIndex = opponent.discardPile.indexOf(context.targets.discardCard);
-                const deadIndex = opponent.discardPile.indexOf(context.targets.discardCard);
+                const discardCard = context.target.find((card) => card.location === 'discard pile');
+                const deadCard = context.target.find((card) => card.location === 'dead pile');
+                const opponent = context.target[0].controller;
+                const discardIndex = opponent.discardPile.indexOf(discardCard);
+                const deadIndex = opponent.discardPile.indexOf(discardCard);
 
-                opponent.discardPile[discardIndex] = context.targets.deadCard;
-                context.targets.deadCard.moveTo('discard pile');
+                opponent.discardPile[discardIndex] = deadCard;
+                deadCard.moveTo('discard pile');
 
-                opponent.deadPile[deadIndex] = context.targets.discardCard;
-                context.targets.discardCard.moveTo('dead pile');
+                opponent.deadPile[deadIndex] = discardCard;
+                discardCard.moveTo('dead pile');
             }
         });
     }
