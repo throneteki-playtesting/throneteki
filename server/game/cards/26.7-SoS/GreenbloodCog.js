edae367de1b38c32b7a5123208fa09307d969f27
Stephen Patane <patane97@gmail.com>
2024-12-23 11:04:31 +1100

    Fixing Greenblood Cog to target 5 or lower

--- server/game/cards/26.7-SoS/GreenbloodCog.js
+++ server/game/cards/26.7-SoS/GreenbloodCog.js
@@ -14,6 +14,7 @@ class GreenbloodCog extends DrawCard {
                 cardCondition: {
                     location: 'play area',
                     type: 'character',
+                    printedCostOrLower: 5,
                     condition: (card) => GameActions.loseIcon({ card }).allow()
                 }
             },
