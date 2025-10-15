fd5ae292530b5a6a6b59513fbec7b4df693aa93a
Stephen Patane <patane97@gmail.com>
2025-10-15 19:04:08 +1100

    Updating Melisandre v1.3.0

--- server/game/cards/26.7-SoS/Melisandre.js
+++ server/game/cards/26.7-SoS/Melisandre.js
@@ -2,29 +2,15 @@ import DrawCard from '../../drawcard.js';
 
 class Melisandre extends DrawCard {
     setupCardAbilities(ability) {
-        this.reaction({
-            when: {
-                afterChallenge: (event) =>
-                    event.challenge.winner === this.controller &&
-                    this.controller.anyCardsInPlay({ participating: true, shadow: true })
-            },
-            target: {
-                cardCondition: (card) =>
-                    card.location === 'play area' && card.getType() === 'character'
-            },
-            limit: ability.limit.perPhase(3),
-            message: '{player} uses {source} to give {target} -1 STR until the end of the phase',
-            handler: (context) => {
-                this.untilEndOfPhase((ability) => ({
-                    match: context.target,
-                    effect: ability.effects.putInShadowsByStrength(-1)
-                }));
-            }
+        this.persistentEffect({
+            match: (card) => card.isParticipating() && !card.isShadow(),
+            targetController: 'any',
+            effect: ability.effects.putInShadowsByStrength(-1)
         });
     }
 }
 
 Melisandre.code = '26501';
-Melisandre.version = '1.2.0';
+Melisandre.version = '1.3.0';
 
 export default Melisandre;
